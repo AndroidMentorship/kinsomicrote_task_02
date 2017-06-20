6d18a76d06e16e8a94c68a402a27c0d81cfc0cb7
kinsomicrote <kinsomicrote.inc@gmail.com>
2017-06-20 17:28:15 +0100

    linked shipping confirmation activity

--- app/src/main/java/com/kingsley/android/kupsfood/EgusiDescriptionActivity.java
+++ app/src/main/java/com/kingsley/android/kupsfood/EgusiDescriptionActivity.java
@@ -1,8 +1,11 @@
 package com.kingsley.android.kupsfood;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class EgusiDescriptionActivity extends Activity {
@@ -13,6 +16,17 @@ public class EgusiDescriptionActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_egusi_description);
+
+    Button guestButton = (Button) findViewById(R.id.guest_order_button);
+
+    guestButton.setOnClickListener(new OnClickListener () {
+
+      @Override
+      public void onClick(View view) {
+        Intent guestIntent = new Intent(EgusiDescriptionActivity.this, ShippingConfirmationActivity.class);
+        startActivity(guestIntent);
+      }
+    });
   }
 
   public void decreaseFoodQuantity(View view){
