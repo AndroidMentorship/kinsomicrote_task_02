b8b7291de488f2ad0ebac3bf34fdd90ee542ab01
kinsomicrote <kinsomicrote.inc@gmail.com>
2017-06-20 17:02:27 +0100

    generated shipping confirmation activity

---
+++ app/src/main/java/com/kingsley/android/kupsfood/ShippingConfirmationActivity.java
@@ -0,0 +1,13 @@
+package com.kingsley.android.kupsfood;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShippingConfirmationActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_shipping_confirmation);
+  }
+}
