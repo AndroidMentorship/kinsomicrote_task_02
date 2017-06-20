e0fa258aabe57b4b55b2556ed333100455d37e4c
kinsomicrote <kinsomicrote.inc@gmail.com>
2017-06-20 16:56:09 +0100

    changed launcher icon and food quantity can now increase and decrease

--- app/src/main/java/com/kingsley/android/kupsfood/EgusiDescriptionActivity.java
+++ app/src/main/java/com/kingsley/android/kupsfood/EgusiDescriptionActivity.java
@@ -2,12 +2,31 @@ package com.kingsley.android.kupsfood;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class EgusiDescriptionActivity extends Activity {
+
+  int quantity = 3;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_egusi_description);
+  }
+
+  public void decreaseFoodQuantity(View view){
+    quantity = quantity - 1;
+    displayQuantity(quantity);
+  }
+
+  public void increaseFoodQuantity(View view){
+    quantity = quantity + 1;
+    displayQuantity(quantity);
+  }
+
+  private void displayQuantity(int foodQuantity) {
+    TextView foodQuantityTextView = (TextView) findViewById(R.id.food_quantity_text_view);
+    foodQuantityTextView.setText("" + foodQuantity);
   }
 }
