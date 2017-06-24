e77b7a909d6b1f279b7de57d5b79a48da54cf15c
kinsomicrote <kinsomicrote.inc@gmail.com>
2017-06-24 15:02:12 +0100

    created food description activity

---
+++ app/src/main/java/com/kingsley/android/kupsfood/activities/FoodDescription.java
@@ -0,0 +1,15 @@
+package com.kingsley.android.kupsfood.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kingsley.android.kupsfood.R;
+
+public class FoodDescription extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_food_description);
+  }
+}
